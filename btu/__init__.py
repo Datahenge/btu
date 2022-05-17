63333b5bf0c500573e882b18008c4f9a158cf522
Brian Pond <brian@datahenge.com>
2022-05-17 16:24:13 -0700

    Add buttons for viewing Failed RQ Jobs, showing details, and deleting them.

--- btu/__init__.py
+++ btu/__init__.py
@@ -9,16 +9,22 @@
 #   https://github.com/meeerp/jobtaskscheduler
 #   Copyright (c) 2015, Codrotech Inc. and contributors
 
-from datetime import datetime  # standard Python library
+from datetime import datetime as DateTimeType # standard Python library
+from datetime import date as DateType
+import json
 import os  # standard Python library
 import re  # standard Python library
 
+from dateutil import parser
+from dateutil.parser._parser import ParserError
 from dateutil.tz import tzutc
 import pytz  # https://pypi.org/project/pytz/
+from rq import Queue
 
 import frappe
+from frappe.utils.background_jobs import get_redis_conn
 
-__version__ = '0.6.0'
+__version__ = '0.7.0'
 
 
 class Result():
@@ -108,7 +114,7 @@ def get_system_datetime_now():
 	"""
 	Return a timezone-aware DateTime value, using the Frappe webserver's System Settings.
 	"""
-	utc_datetime = datetime.now(tzutc())  # Get the current UTC datetime.
+	utc_datetime = DateTimeType.now(tzutc())  # Get the current UTC datetime.
 	return utc_datetime.astimezone( get_system_timezone())  # Convert to the site's Time Zone:
 
 
@@ -144,3 +150,131 @@ def dprint(msg, check_env=None, force=None):
 		print(msg)
 	elif is_env_var_set(check_env):
 		print(msg)
+
+
+def date_to_iso_string(any_date):
+	"""
+	Given a date, create an ISO String.  For example, 2021-12-26.
+	"""
+	if not isinstance(any_date, DateType):
+		raise Exception(f"Argument 'any_date' should have type 'datetime.date', not '{type(any_date)}'")
+	return any_date.strftime("%Y-%m-%d")
+
+
+def iso_string_to_date(any_string):
+	"""
+	Converts an ISO string into a datetime.date
+	"""
+	if isinstance(any_string, DateTimeType):
+		return any_string.date()
+	elif isinstance(any_string, DateType):
+		return any_string
+	return parser.parse(any_string).date()
+
+
+def rq_job_to_dict(rq_job):
+	"""
+	Given a Python RQ job, create a Dictionary of values that is display-friendly.
+	"""
+	# Ignoring these fields:  data
+
+	result = {
+		"job_id": rq_job._id,  # pylint: disable=protected-access
+		"created_at": date_to_iso_string(rq_job.created_at),
+		"function_name": rq_job.func_name,
+		"instance": rq_job._instance,  # pylint: disable=protected-access
+		# "args": rq_job._args,  # pylint: disable=protected-access
+		#"kwargs": rq_job._kwargs,  # pylint: disable=protected-access
+		"description": rq_job.description,
+		"origin": rq_job.origin,
+		"datetime_enqueued": date_to_iso_string(rq_job.enqueued_at) if rq_job.enqueued_at else None,
+		"datetime_started": date_to_iso_string(rq_job.started_at) if rq_job.started_at else None,
+		"datetime_ended": date_to_iso_string(rq_job.ended_at) if rq_job.ended_at else None,
+		"result": str(rq_job._result),  # pylint: disable=protected-access
+		"execution_info": str(rq_job.exc_info),  # NOTE: This can be a very large amount of string text.
+		"timeout":	rq_job.timeout,
+		"result_ttl": rq_job.result_ttl,
+		"failure_ttl": rq_job.failure_ttl,
+		"ttl": rq_job.ttl,
+		"worker_name":	rq_job.worker_name,
+		"status":	rq_job._status,  # pylint: disable=protected-access
+		#"dependency_ids": rq_job._dependency_ids,  # pylint: disable=protected-access
+		#"meta":	rq_job.meta,
+		"serializer":	rq_job.serializer.__name__,
+		"retries_left":	rq_job.retries_left,
+		"retry_intervals":	rq_job.retry_intervals,
+		"redis_server_version":	rq_job.redis_server_version,
+		"last_heartbeat":  date_to_iso_string(rq_job.last_heartbeat),
+	}
+	#for each_key, each_value in rq_job_dict.items():
+	#	print(f"key = {each_key}, value type = {type(each_value)}")
+	return result
+
+
+@frappe.whitelist()
+def list_failed_jobs():
+	"""
+	List all RQ Jobs that are marked as fail inside a queue.
+	"""
+	conn = get_redis_conn()
+	queues = Queue.all(conn)
+	failed_jobs = []
+	message = "Failed Jobs:<br><br>"
+
+	for each_queue in queues:
+		fail_registry = each_queue.failed_job_registry
+		for job_id in fail_registry.get_job_ids():
+			job = each_queue.fetch_job(job_id)
+			failed_jobs.append(job_id)
+			message += f"Queue={each_queue.name} : Job={job_id} : {job.description}<br>"
+	frappe.msgprint(message)
+
+@frappe.whitelist()
+def print_job_details(queue_name, job_id):
+	"""
+	Given the name of a Queue and Job, fetch details about the Job.
+	"""
+	conn = get_redis_conn()
+	this_queue_key = "rq:queue:" + queue_name
+	this_queue = Queue.from_queue_key(queue_key=this_queue_key, connection=conn)
+
+	this_job = this_queue.fetch_job(job_id)
+	if not this_job:
+		frappe.msgprint(f"Unable to find RQ Job with identifier = '{job_id}' in queue named '{queue_name}'")
+	else:
+		rq_job_dict = rq_job_to_dict(this_job)
+		#for each_key, each_value in rq_job_dict.items():
+		#	print(f"key = {each_key}, value type = {type(each_value)}")
+		prettier_string = json.dumps(rq_job_dict, indent=4)
+		prettier_string = prettier_string.replace("\n", "<br>")
+		# print(prettier_string)
+		frappe.msgprint(prettier_string)
+
+@frappe.whitelist(methods=['DELETE'])
+def remove_failed_jobs(date_from, date_to, wildcard_text=None):
+	"""
+	Delete RQ Jobs from the Redis database, with filters for dates and text.
+	"""
+
+	# Convert JS arguments to dates:
+	date_from = iso_string_to_date(date_from)
+	date_to = iso_string_to_date(date_to)
+
+	conn = get_redis_conn()
+	queues = Queue.all(conn)
+
+	jobs_deleted = 0
+	frappe.msgprint(f"Searching for Failed Jobs from {date_from} to {date_to} ...")
+	for each_queue in queues:
+		fail_registry = each_queue.failed_job_registry
+		for job_id in fail_registry.get_job_ids():
+			job = each_queue.fetch_job(job_id)
+			if job.last_heartbeat and (job.last_heartbeat.date() >= date_from) and (job.last_heartbeat.date() <= date_to):
+				# Delete this job:
+				fail_registry.remove(job, delete_job=True)
+				jobs_deleted += 1
+
+	if not jobs_deleted:
+		frappe.msgprint("No RQ Jobs found that match this criteria.")
+	else:
+		frappe.msgprint(f"{jobs_deleted} jobs deleted from the Redis Queue.")
--- btu/btu_core/doctype/btu_configuration/btu_configuration.js
+++ btu/btu_core/doctype/btu_configuration/btu_configuration.js
@@ -1,8 +1,129 @@
+// btu_configuration.js
+
 // Copyright (c) 2022, Datahenge LLC and contributors
 // For license information, please see license.txt
 
 frappe.ui.form.on('BTU Configuration', {
-	// refresh: function(frm) {
 
-	// }
+	refresh: function(frm) {
+		frm.events.add_button_list_failed_jobs(frm);
+		frm.events.add_button_describe_job(frm);
+		frm.events.add_button_delete_failed_jobs(frm);
+	}
+
+	,add_button_list_failed_jobs: function (frm) {
+		// This function adds a button "List Failed RQ Jobs"
+		frm.add_custom_button(__('List Failed RQ Jobs'), () => {
+			frappe.call({
+					type:"GET",
+					method:"btu.list_failed_jobs",
+				}).done(() => {
+					console.log("Done");
+					frm.reload_doc();
+				}).fail(() => {
+					frappe.msgprint(__("Could not generate Secret"));
+				});
+		}, 'Redis Queue');
+	}
+
+	,add_button_describe_job: function (frm) {
+
+		frm.add_custom_button(__('Describe Job'), () => {
+
+			var my_dialog = new frappe.ui.Dialog({
+				title: 'Describe Job',
+				width: 100,
+				fields: [
+					{
+						'fieldtype': 'Data',
+						'fieldname': 'queue_name',
+						'label': __('Queue'),
+						'default': 'default',
+						reqd: 1
+					},
+					{
+						'fieldtype': 'Data',
+						'fieldname': 'job_id',
+						'label': __('Job Id'),					
+						reqd: 1
+					}
+				]
+			});
+
+			my_dialog.set_primary_action(__('Show'), args => {
+
+				frappe.call({
+					type:"POST",
+					method:"btu.print_job_details",
+					args: {
+						"queue_name": args.queue_name,
+						"job_id": args.job_id
+					}
+				}).done(() => {
+					my_dialog.hide();
+				}).fail(() => {
+					my_dialog.hide();
+				});
+			});
+
+			my_dialog.show();
+
+		}, 'Redis Queue');
+	}
+
+	,add_button_delete_failed_jobs: function (frm) {
+		// This function adds a button "Deleted Failed RQ Jobs"
+		frm.add_custom_button(__('Delete Failed RQ Jobs'), () => {
+
+			var my_dialog = new frappe.ui.Dialog({
+				title: 'Delete Failed RQ Jobs',
+				width: 100,
+				fields: [
+					{
+						'fieldtype': 'Date',
+						'fieldname': 'date_from',
+						'label': __('From Date'),
+						'default': frappe.datetime.add_days(frappe.datetime.get_today(), -28),
+						reqd: 1
+					},
+					{
+						'fieldtype': 'Date',
+						'fieldname': 'date_to',
+						'label': __('To Date'),
+						'default': frappe.datetime.add_days(frappe.datetime.get_today(), -1),
+						reqd: 1
+					},
+					{
+						'fieldtype': 'Data',
+						'fieldname': 'wildcard_text',
+						'label': __('Wildcard Text'),					
+						reqd: 0
+					}
+
+				]
+			});
+
+			my_dialog.set_primary_action(__('Run'), args => {
+
+				frappe.call({
+					type:"DELETE",
+					method:"btu.remove_failed_jobs",
+					args: { 
+						"date_from": args.date_from,
+						"date_to": args.date_to,
+						"wildcard_text": args.wildcard_text
+					}
+				}).done(() => {
+					my_dialog.hide();
+				}).fail(() => {
+					my_dialog.hide();
+				});
+			});
+
+			my_dialog.show();
+
+		}, 'Redis Queue');
+
+	}  //end of function
+
 });
