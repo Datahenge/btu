844eef8e24000af6bf3dca84ce11a155aba9be73
Brian Pond <brian@datahenge.com>
2024-12-14 11:49:09 -0800

    Remove frappe.whitelist() from static methods in SchedulerAPI

--- btu/btu_api/scheduler.py
+++ btu/btu_api/scheduler.py
@@ -21,7 +21,6 @@ class SchedulerAPI():
 	Static methods are for external use.
 	"""
 
-	@frappe.whitelist()
 	@staticmethod
 	def send_ping():
 		"""
@@ -30,7 +29,6 @@ class SchedulerAPI():
 		response = SchedulerAPI().send_message(RequestType.ping, content=None)
 		return response
 
-	@frappe.whitelist()
 	@staticmethod
 	def reload_task_schedule(task_schedule_id):
 		"""
