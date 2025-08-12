e16eb071cf1e969f3575de1dcde99ce2c8ca6ae2
Brian Pond <brian@datahenge.com>
2025-08-12 10:51:25 -0700

    Invented new concept of BTU Task Data for storing large objects for later use by background workers.

---
+++ btu/btu_core/doctype/btu_task_data/btu_task_data.py
@@ -0,0 +1,13 @@
+# Copyright (c) 2025, Datahenge LLC and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe.model.document import Document
+
+
+class BTUTaskData(Document):
+	pass
+
+	@frappe.whitelist()
+	def button_readme_clicked(self):
+		frappe.msgprint("BTU Task Data has a DocField named 'text_data'.  But it could be gigantic.<br>To prevent web browser issues, it is always hidden.")
---
+++ btu/btu_core/doctype/btu_task_data/test_btu_task_data.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Datahenge LLC and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestBTUTaskData(FrappeTestCase):
+	pass
